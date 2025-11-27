c342ca785c5f1d47798b92786e86ebae81f63345
Ignacio <ignaciograner@gmail.com>
2025-11-27 11:04:14 -0600

    Basic audio methods to test functionality

--- arm/include/audio_controller.h
+++ arm/include/audio_controller.h
@@ -44,7 +44,49 @@ typedef struct {
 } audio_controller_t;
 
 
+/* ====================
+ * PUBLIC API
+ * ==================== */
 
+/**
+ * @brief Initialize audio controller
+ *
+ * @param ctrl Pointer to audio_controller_t structure
+ * @param fifo Pointer to initialized FIFO context;
+ * @return 0 on success, -1 on error
+ */
+int audio_controller_init(audio_controller_t *ctrl, fifo_context_t *fifo);
+
+
+/**
+ * @brief Shutdown audio controller
+ *
+ * Stops playback and cleans up resources
+ *
+ * @param ctrl Pointer to audio_controler_t structure
+ */
+int audio_controller_shutdown(audio_controller_t *ctrl);
+
+/**
+ * @brief Load playlist from directory
+ *
+ * Scans directory for WAV files and adds them to the playlist
+ *
+ * @param ctrl Pointer to audio_controller_t structure;
+ * @param directory Path to music directory
+ * @return Number of of songs loaded, -1 on error
+ */
+int audio_controller_load_playlist(audio_controller_t *ctrl, const char *directory);
+
+/**
+ * @brief Get current playback time
+ *
+ * @param ctrl Pointer to audio_controller_t structure
+ * @param current_sec Output: current time in seconds
+ * @param total_sec Output: total duration in seconds
+ */
+void audio_controller_get_time(audio_controller_t *ctrl, uint32_t *current_sec, uint32_t *total_sec);
 
 
 #endif /* AUDIO_CONTROLLER_H */
+
